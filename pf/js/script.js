ec70118d781ae1932fca9cc042d6071107788327
engr-inya <iansantospa@gmail.com>
2025-03-28 21:54:39 +0800

    Add files via upload

---
+++ pf/js/script.js
@@ -0,0 +1,7 @@
+/* ==== typing animation ==== */
+var typed = new Typed(".typing" ,{
+    strings:["2nd Year Student","BS Computer Engineering"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
